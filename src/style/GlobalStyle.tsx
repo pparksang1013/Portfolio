d86ad8302b3b02b58936d747f42a614992d3f2e8
pparksang <pparksang1013@gmail.com>
2024-01-09 16:41:35 +0900

    💅STYLE

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,38 +1,32 @@
 import { createGlobalStyle } from "styled-components";
-import happinessTitle from "../assets/font/Happiness-Sans-Title.woff2";
-import happinessBold from "../assets/font/Happiness-Sans-Bold.woff2";
-import happinessReg from "../assets/font/Happiness-Sans-Regular.woff2";
+import wantedSans from "../assets/font/WantedSansVariable.woff2";
+import akira from "../assets/font/AkiraExpandedDemo.otf";
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'happiness';
-        src: url(${happinessReg}) format('woff2');
-        font-weight: 400;
+        font-family: 'wanted';
+        src: url(${wantedSans}) format('woff2-variations');
+        font-weight: 100 900;
         font-style: normal;
     }
 
     @font-face {
-        font-family: 'happiness';
-        src: url(${happinessBold}) format('woff2');
-        font-weight: 700;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'happiness';
-        src: url(${happinessTitle}) format('woff2');
+        font-family: "akira";
+        src: url(${akira});
         font-weight: 900;
-        font-style: normal;
     }
 
     body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    background: #f2f2f2;
-    font-family: "happiness";
+    font-family: "wanted";
     font-weight: 900;
     font-size: 4rem;
+
     }
 
+    h1,h2,h3,h4,h5,h6 {
+        margin: 0;
+    }
 `;
