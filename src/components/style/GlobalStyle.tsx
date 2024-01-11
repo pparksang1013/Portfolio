ef3b87221b7f52dacfb131ae29d199a810cde492
pparksang <pparksang1013@gmail.com>
2024-01-12 02:06:56 +0900

    🧩REFACTORING 및⭐️FEAT
    🧩REFACTORING
    - 경로변경
    
    ⭐️FEAT
    - CSS 변수 설정

--- src/components/style/GlobalStyle.tsx
+++ src/components/style/GlobalStyle.tsx
@@ -1,11 +1,11 @@
 import { createGlobalStyle } from "styled-components";
-import wantedSans from "../../assets/font/WantedSansVariable.woff2";
+import suit from "../../assets/font/SUIT-Variable.woff2";
 import akira from "../../assets/font/AkiraExpandedDemo.otf";
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'wanted';
-        src: url(${wantedSans}) format('woff2-variations');
+        font-family: 'suit';
+        src: url(${suit}) format('woff2-variations');
         font-weight: 100 900;
         font-style: normal;
     }
@@ -16,14 +16,19 @@ export const GlobalStyle = createGlobalStyle`
         font-weight: 900;
     }
 
-    body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "wanted";
-    font-weight: 900;
-    font-size: 4rem;
+    :root {
+        --primary-color: #f20505;
+        --primary-rgb: 217, 4, 22;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
+    body {
+        font-family: "suit";
     }
 
     h1,h2,h3,h4,h5,h6 {
