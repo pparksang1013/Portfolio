8f01e900aec843ced3410f0bc4d660a345b9ff10
pparksang <pparksang1013@gmail.com>
2024-01-15 23:46:10 +0900

    💅STYLE & 🧩REFACTORING & ⭐️FEAT
    🧩REFACTORING
    - 컴포넌트 디렉토리 변경으로 인한 경로변경
    
    ⭐️FEAT
    - ProjectComp import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import MainComp from "./components/main/MainComp";
+import MainComp from "./components/main_page/MainComp";
+import AboutComp from "./components/about_page/AboutComp";
+import ProjectComp from "./components/project_page/ProjectComp";
+
+import styled from "styled-components";
 
 function App() {
     return (
-        <>
+        <Style_Root>
             <MainComp />
-            <div>ABOUT ME</div>
+            <AboutComp />
+            <ProjectComp />
             <div>PROJECT 1. 투두리스트 + 로그인 2. 칸반보드 3. 블로그 4. 아워맵 하</div>
-        </>
+        </Style_Root>
     );
 }
 
 export default App;
+
+const Style_Root = styled.div`
+    scroll-snap-type: y mandatory;
+`;
