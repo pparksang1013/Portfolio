f1d1f88cefa2eb1a196c22cb03ae62dcde12d635
pparksang <pparksang1013@gmail.com>
2024-01-19 20:11:06 +0900

    ⭐️FEAT 아이콘 데이터 추가

--- src/components/project_page/project_data.ts
+++ src/components/project_page/project_data.ts
@@ -1,9 +1,16 @@
 interface ProjectType {
     title: string;
+    icon: string;
 }
 
+import todoIcon from "/img/todo-login_icon.png";
+import kanbanIcon from "/img/kanban_icon.png";
+import blogIcon from "/img/blog_icon.png";
+import portfolioIcon from "/img/portfolio_icon.png";
+
 export const projects_data: ProjectType[] = [
-    { title: "로그인과 투두리스트" },
-    { title: "칸반보드" },
-    { title: "블로그" },
+    { title: `LOGIN & TODOLIST`, icon: todoIcon },
+    { title: "KANBAN BOARD", icon: kanbanIcon },
+    { title: "BLOG", icon: blogIcon },
+    { title: "PORTFOLIO", icon: portfolioIcon },
 ];
