312b31a522da2310f38306621d34852a293d0dc9
pparksang <pparksang1013@gmail.com>
2024-02-15 16:18:10 +0900

    🧩REFACTORING Prop 추가

--- src/components/common_comp/H3Comp.tsx
+++ src/components/common_comp/H3Comp.tsx
@@ -1,19 +1,26 @@
 import { styled } from "styled-components";
 
 interface H3Interface {
-    title: string;
+    title?: string;
     color: string;
+    fontsize: string;
+    stroke?: boolean;
 }
 
-function H3Comp({ title, color }: H3Interface) {
-    return <Style_H3 color={color}>{title}</Style_H3>;
+function H3Comp({ title, color, fontsize, stroke }: H3Interface) {
+    return (
+        <Style_H3 color={color} fontsize={fontsize} stroke={stroke}>
+            {title}
+        </Style_H3>
+    );
 }
 
 export default H3Comp;
 
-const Style_H3 = styled.h3`
-    font-size: 2.4rem;
-    font-weight: 900;
-    margin-bottom: 1.4rem;
+const Style_H3 = styled.h3<H3Interface>`
+    font-size: ${(props) => props.fontsize};
+    margin-bottom: 0.6rem;
+    font-family: "akira";
     color: ${(props) => props.color};
+    -webkit-text-stroke: ${(props) => (props.stroke ? "1.6px" : "none")};
 `;
