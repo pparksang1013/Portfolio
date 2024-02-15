acbcd7677cdc40ec6df9d3fb2548a351426edc0f
pparksang <pparksang1013@gmail.com>
2024-02-15 16:41:54 +0900

    ⭐️FEAT Insight title comp
    - 인사이트 컴포넌트 안에서 사용하는 커먼컴포넌트

---
+++ src/components/common_comp/InsightTitle.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+interface StyleProps {
+    eng?: boolean;
+}
+
+function InsightTitle({ title, i, eng }: { title: string; i: string; eng?: boolean }) {
+    return (
+        <Style_Box>
+            <Style_InsightIndex>{i}</Style_InsightIndex>
+            <Style_InsightTitle eng={eng}>{title}</Style_InsightTitle>
+        </Style_Box>
+    );
+}
+
+export default InsightTitle;
+
+const Style_Box = styled.div`
+    display: flex;
+    align-items: flex-end;
+    margin: 2rem 0 0.4rem;
+
+    &:first-of-type {
+        margin: 0 0 0.4rem;
+    }
+`;
+
+const Style_InsightIndex = styled.span`
+    font-size: 1.1rem;
+    -webkit-text-stroke: 1.4px;
+    color: var(--primary-color);
+    font-weight: 900;
+    letter-spacing: 1.1px;
+`;
+
+const Style_InsightTitle = styled.h4<StyleProps>`
+    color: var(--black_color);
+    font-size: 1.1rem;
+    font-weight: 800;
+    width: fit-content;
+    margin-left: 4px;
+    text-transform: uppercase;
+    -webkit-text-stroke: ${(props) => (props.eng ? "1.2px" : "0.5px")};
+`;
