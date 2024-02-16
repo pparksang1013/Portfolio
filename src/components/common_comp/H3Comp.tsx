bf6b9ace4423e6c2af29929c56d7baf98cce79e6
pparksang <pparksang1013@gmail.com>
2024-02-16 14:46:01 +0900

    🔥FIX Warning
    - React does not recognize the colorProp prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase colorprop instead. If you accidentally passed it from a parent component, remove it from the DOM element.

--- src/components/common_comp/H3Comp.tsx
+++ src/components/common_comp/H3Comp.tsx
@@ -1,16 +1,27 @@
 import { styled } from "styled-components";
 
 interface H3Interface {
-    title?: string;
+    $color: string;
+    $fontsize: string;
+    $stroke?: boolean;
+    $marginTop?: string | undefined;
+}
+
+function H3Comp({
+    title,
+    color,
+    fontsize,
+    stroke,
+    marginTop,
+}: {
+    title: string;
     color: string;
     fontsize: string;
     stroke?: boolean;
     marginTop?: string | undefined;
-}
-
-function H3Comp({ title, color, fontsize, stroke, marginTop }: H3Interface) {
+}) {
     return (
-        <Style_H3 color={color} fontsize={fontsize} stroke={stroke} marginTop={marginTop}>
+        <Style_H3 $color={color} $fontsize={fontsize} $stroke={stroke} $marginTop={marginTop}>
             {title}
         </Style_H3>
     );
@@ -19,10 +30,10 @@ function H3Comp({ title, color, fontsize, stroke, marginTop }: H3Interface) {
 export default H3Comp;
 
 const Style_H3 = styled.h3<H3Interface>`
-    font-size: ${(props) => props.fontsize};
-    margin-top: ${(props) => props.marginTop};
+    font-size: ${(props) => props.$fontsize};
+    margin-top: ${(props) => props.$marginTop};
     margin-bottom: 0.6rem;
     font-family: "akira";
-    color: ${(props) => props.color};
-    -webkit-text-stroke: ${(props) => (props.stroke ? "1.6px" : "none")};
+    color: ${(props) => props.$color};
+    -webkit-text-stroke: ${(props) => (props.$stroke ? "1.6px" : "none")};
 `;
