40398a7193c9354d42a17af88b133de2db5dc5ba
pparksang <pparksang1013@gmail.com>
2024-03-11 16:44:49 +0900

    💅STYLE
    - 메인컬러 변경
    - fontface 추가

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components";
+import minsans from "/font/MinSansVF.ttf";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "minsans";
+        src: url(${minsans}) format('woff2-variations');
+        font-weight: 100 900;
+    }
+
+
     :root {
         --black-color: #454545;
-        --primary-color: #24D3B1;
-        --primary-rgb: 36, 211, 177;
-        --blue-color: #4078D3;
+        --primary-color: #8091F2;
+        --primary-rgb: 128, 145, 242;
         --yellow-color: #FFE385;
         --orange-color: #ff6c4f;
     }
@@ -17,9 +24,8 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        font-family: "suit";
-        overflow-x: hidden;
-        color: var(--black-color);
+        font-family: "minsans";
+        background-color: rgb(251,251,251);
     }
 
     h1,h2,h3,h4,h5,h6 {
@@ -34,7 +40,6 @@ export const GlobalStyle = createGlobalStyle`
     a {
         text-decoration: none;
     }
-
 
     ::-webkit-scrollbar {
         display: none; 
