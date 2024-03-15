04ddf342033714d238c9fdc01545e370fac20a16
pparksang <pparksang1013@gmail.com>
2024-03-15 19:23:25 +0900

    ⭐️FEAT 메인페이지 관련 데이터 추가

--- src/data/home/home.ts
+++ src/data/home/home.ts
@@ -3,10 +3,16 @@ interface IHomeData {
     title: string;
     index: number;
     link: string;
+    img: IImg;
+}
+
+interface IImg {
+    path: string;
+    height: number;
 }
 
 export const homedata: IHomeData[] = [
-    { id: 0, title: "ABOUT", index: 1, link: "/about" },
-    { id: 1, title: "PROJECT", index: 2, link: "/project" },
-    { id: 2, title: "CONTACT", index: 3, link: "/contact" },
+    { id: 0, title: "ABOUT", index: 1, link: "/about", img: { path: "img/about.png", height: 40 } },
+    { id: 1, title: "PROJECT", index: 2, link: "/project", img: { path: "img/project.png", height: 90 } },
+    { id: 2, title: "CONTACT", index: 3, link: "/contact", img: { path: "img/contact.png", height: 30 } },
 ];
