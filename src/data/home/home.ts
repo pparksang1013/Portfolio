19211c9d47bb07c1c989b157617c267e03ed1f28
pparksang <pparksang1013@gmail.com>
2024-03-20 16:31:29 +0900

    🧩REFACTORING 데이터 구조 변경

--- src/data/home/home.ts
+++ src/data/home/home.ts
@@ -3,16 +3,10 @@ interface IHomeData {
     title: string;
     index: number;
     link: string;
-    img: IImg;
-}
-
-interface IImg {
-    path: string;
-    height: number;
 }
 
 export const homedata: IHomeData[] = [
-    { id: 0, title: "ABOUT", index: 1, link: "/about", img: { path: "img/about.png", height: 40 } },
-    { id: 1, title: "PROJECT", index: 2, link: "/project", img: { path: "img/project.png", height: 90 } },
-    { id: 2, title: "CONTACT", index: 3, link: "/contact", img: { path: "img/contact.png", height: 30 } },
+    { id: 0, title: "ABOUT", index: 1, link: "/about" },
+    { id: 1, title: "PROJECT", index: 2, link: "/project" },
+    { id: 2, title: "CONTACT", index: 3, link: "/contact" },
 ];
