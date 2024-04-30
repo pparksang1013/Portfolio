f5bb030e713a93310179e11b79eaa98446d1630d
pparksang <pparksang1013@gmail.com>
2024-04-30 17:20:50 +0900

    ⭐️FEAT 스크롤 이벤트 repeat 설정

--- src/components/header/HeaderBox.tsx
+++ src/components/header/HeaderBox.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { headerBoxStyle } from "./style/HeaderBoxStyle";
-import { useScroll, useTransform } from "framer-motion";
+import { useScroll, useTransform, useMotionValueEvent } from "framer-motion";
 
 export const HeaderBox = ({ children, shouldAnimate }: { children: React.ReactNode; shouldAnimate?: boolean }) => {
+    const [animateRepeat, setAnimateRepeat] = useState(false);
     const { scrollY } = useScroll();
+    const once = animateRepeat || shouldAnimate;
     const opacity = useTransform(scrollY, [0, 1], [0, 1]);
 
-    return <headerBoxStyle.Box style={{ opacity: shouldAnimate ? 1 : opacity }}>{children}</headerBoxStyle.Box>;
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        if (latest > 2) {
+            setAnimateRepeat(true);
+            return;
+        } else {
+            return;
+        }
+    });
+
+    return <headerBoxStyle.Box style={{ opacity: once ? 1 : opacity }}>{children}</headerBoxStyle.Box>;
 };
